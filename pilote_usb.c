38e31038b5fc389ed379e62d1360947626335f9b
Freddy Hidalgo-Monchez <freddy.hidalgo.m@gmail.com>
2018-11-12 15:42:49 -0500

    define init, exit, and write probe structure

--- pilote_usb.c
+++ pilote_usb.c
@@ -11,9 +11,8 @@
 #include <linux/kernel.h>
 #include <linux/kthread.h>
 #include <linux/cdev.h>
-#include <linux/spinlock.h>
-#include <linux/cred.h>
-#include <linux/semaphore.h>
+#include <linux/usb.h>
+#include <linux/usb/video.h>
 
 MODULE_AUTHOR("Freddy Hidalgo-Monchez");
 MODULE_LICENSE("Dual BSD/GPL");
@@ -21,15 +20,30 @@ MODULE_LICENSE("Dual BSD/GPL");
 static ssize_t module_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos);
 static int module_open(struct inode *inode, struct file *filp);
 static int module_release(struct inode *inode, struct file *filp);
+static int module_probe(struct usb_interface *intf, const struct usb_device_id *devid);
 
-static struct file_operations myModule_fops = {
-	.owner 	 = THIS_MODULE,
+// struct table containing list of all the different kind of USB devices
+// driver can accept
+// makes sure the callback function in the USB driver is called
+static struct usb_device_id pilote_usb_id [] = {
+    {USB_DEVICE(0x046d, 0x08cc)},
+    {}
+};
+
+MODULE_DEVICE_TABLE(usb, pilote_usb_id);
+
+static struct usb_driver pilote_usb_driver = {
+	.name	 = "myUSBModule",
+    .id_table = pilote_usb_id,
+    .probe = module_probe,
+}; 
+
+static struct file_operations myUSBModule_fops = {
+	.owner 	 = THIS_MODULE,  
 	.read	 = module_read,
 	.open	 = module_open,
 	.release = module_release,
 };
-
-
 
 static int  pilote_init (void);
 static void pilote_exit (void);
@@ -39,19 +53,30 @@ static int module_open(struct inode *inode, struct file *filp) {
 	return 0;
 }
 
-
 static int module_release(struct inode *inode, struct file *filp) {
     printk(KERN_WARNING"Pilote RELEASE : Hello, world\n");
     return 0;
 }
 
-
-static __init int pilote_init(void)	{
-    printk(KERN_WARNING "Pilote : Hello, world (Pilote)\n");
+static int module_probe(struct usb_interface *intf, const struct usb_device_id *devid){
+    printk("PROBE");
     return 0;
 }
 
+static __init int pilote_init(void)	{
+
+    int retval = usb_register(&pilote_usb_driver);
+
+    if(retval){
+        printk("usb_register failed. Error number %d", retval);
+    }
+
+    printk(KERN_WARNING "Pilote : Hello, world (Pilote)\n");
+    return retval;
+}
+
 static void __exit pilote_exit(void){
+    usb_deregister(&pilote_usb_driver);
     printk(KERN_ALERT "Pilote: Goodbye, cruel world\n");
 }
 
